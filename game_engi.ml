87931b4c10807c19d4249e997d1ca18f4cb7173b
Julien Meslé <julien.mesle@gmail.com>
2023-07-12 14:20:12 +0200

    Added system for detecting winning moves for specified player

--- game_engi.ml
+++ game_engi.ml
@@ -1,14 +1,36 @@
 (* to handle the game parts : changing symbol on a case, finding next move for computer, detecting win, etc. *)
 
+open Gui
+
+(* ------ CONSTANTS ------ *)
+
 let grid_size = 600
 let blank = 0
 let player1 = 1
 let player2 = 2
 let curr_player = ref player1
 
-let grid = [|player1; blank; blank;
+let winLines = [|
+	[|[|1;2|]; [|3;6|]; [|4;8|]|];
+	[|[|0;2|]; [|4;7|]|];
+	[|[|0;1|]; [|4;6|]; [|5;8|]|];
+	[|[|0;6|]; [|4;5|]|];
+	[|[|1;7|]; [|3;5|]; [|0;8|]; [|6;2|]|];
+	[|[|3;4|]; [|2;8|]|];
+	[|[|0;3|]; [|2;4|]; [|7;8|]|];
+	[|[|1;4|]; [|6;8|]|];
+	[|[|6;7|]; [|0;4|]; [|2;5|]|]
+|]
+
+exception IsWinning
+
+
+(* ------ CODE ------ *)
+
+let grid = [|blank; blank; blank;
 			blank; blank; blank;
 			blank; blank; blank|]
+
 
 (* gives the id of the square (from 0 to 8) corresponding to the position *)
 let pos_to_square_id x y = 
@@ -22,16 +44,25 @@ let pos_to_square_id x y =
 	else ytmp := 2);
 	!xtmp + 3 * !ytmp)
 
-let get_symbol id = grid.(id)
-
 let opponent color = 
 	if color = player1 then player2 else player1
+
+
+(* check if the board is currently a winning position for the given player *)
+let is_winning_move move player board = 
+	let is_winning = ref false in
+			for i=0 to (Array.length (winLines.(move)) -1) do
+				let line = winLines.(move).(i) in
+					if player = board.(line.(0)) && player = board.(line.(1)) then
+					begin is_winning := true; Printf.printf "victoire par %d %d" line.(0) line.(1) end
+			done;
+			!is_winning
 
 let play_square x y = 
 	let id = pos_to_square_id x y in 
 	Printf.printf "Position du clic : (%d, %d) / id = %d" x y id; print_newline ();
-	if id > 0 then begin
-		if grid.(id) = blank then 
-			begin grid.(id) <- !curr_player; curr_player := opponent !curr_player end
+	if id >= 0 then begin
+		if grid.(id) = blank then begin 
+			if is_winning_move id !curr_player grid then begin Printf.printf "Victoire du joueur %d" !curr_player; print_newline () end;
+			grid.(id) <- !curr_player; curr_player := opponent !curr_player end
 		end
-
--- gui.ml
+++ gui.ml
@@ -15,14 +15,33 @@ let draw_grid () =
 
 
 let draw_symbols () =
+	for j=2 downto 0 do
+		for i=0 to 2 do
+			(if grid.(i+3*j) = player1 then Printf.printf "X|"
+			else if grid.(i+3*j) = player2 then Printf.printf "O|"
+			else Printf.printf " |");
+		done;
+		Printf.printf "\n"
+	done;
+	print_newline ();
 	for i=0 to 2 do
 		for j=0 to 2 do
-			(if grid.(i+3*j) = player1 then Printf.printf "joueur1"
-			else if grid.(i+3*j) = player2 then Printf.printf "joueur2"
-			else Printf.printf "blanc");
+			if grid.(i+3*j) = blank then begin (* on remplit la case de blanc *)
+				set_color background;
+				fill_rect (1 + i*grid_size/3) (1 + j*grid_size/3) (grid_size/3 - 1) (grid_size/3 - 1) end
+			else 
+				let x0 = grid_size/6 * (2*i+1) and y0 = grid_size/6 * (2*j+1) in
+				if grid.(i+3*j) = player1 then begin (* si c'est joueur1 : on dessine une croix *)
+					set_color black;
+					let segm_croix = [|x0 - grid_size/12, y0 - grid_size/12, x0 + grid_size/12, y0 + grid_size/12;
+									   x0 - grid_size/12, y0 + grid_size/12, x0 + grid_size/12, y0  - grid_size/12|] in
+						draw_segments segm_croix end
+				else begin  (* si c'est joueur2 : on dessine un cercle *)
+					set_color black;
+					draw_circle x0 y0 (grid_size/12)
+				end
 		done
-	done;
-	print_newline ()
+	done
 
 let draw_scoreboard () = 
 	()
@@ -37,7 +56,7 @@ let rec interactive () =
   let event = wait_next_event [Key_pressed; Button_down] in
   if event.keypressed && event.key == 'q' then exit 0
 	else if event.keypressed then begin print_char event.key; print_newline () end
-  else if event.button then begin (Game_engi.play_square event.mouse_x event.mouse_y); draw_symbols () end;
+  else if event.button then begin (Game_engi.play_square event.mouse_x event.mouse_y); draw_symbols (); draw_grid () end;
   interactive ()
 
 
